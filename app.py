9a99607448bcff6cb0db8169df1b2f0281dec78a
isaic <isaiccruse28@gmail.com>
2026-08-25 02:10:38 -0700

    Added Reservation price and Bid/Ask prices

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ def update_seed():
 
 @dataclass
 class model_parameters:
+    simulations : int
     s : float
     T : float
     sigma : float
@@ -27,6 +28,7 @@ class model_parameters:
     cash : float
 
 def get_slider_params() -> model_parameters:
+    simulations : int = st.sidebar.slider(label="Simulation Count", min_value=1, max_value=1000)
     s : float = st.sidebar.slider(label="Inital Price", min_value=0, max_value=100, value=100, on_change=update_seed)
     T : float = st.sidebar.slider(label="Time Horizon", min_value=0.1, max_value=10.0, value=1.0)
     sigma : float = st.sidebar.slider(label="Volatility", min_value=0.1, max_value=10.0, value=1.0)
@@ -36,17 +38,36 @@ def get_slider_params() -> model_parameters:
     gamma : float = st.sidebar.slider(label="Gamma", min_value=0.1, max_value=1.0, value=0.1)
     k : float = st.sidebar.slider(label="k", min_value=0.1, max_value=2.0, value=1.5)
     M : float = st.sidebar.slider(label="M", min_value=0.0, max_value=1.0, value=0.5)
-    cash : float = st.sidebar.slider(label="Volatility", min_value=0.0, max_value=100.0, value=0.0)
-    return model_parameters(s,T,sigma,N,q,gamma,k,M,cash)
+    cash : float = st.sidebar.slider(label="Cash", min_value=0.0, max_value=100.0, value=0.0)
+    return model_parameters(simulations,s,T,sigma,N,q,gamma,k,M,cash)
 
 def run_avellaneda_model(p : model_parameters):
     var = p.sigma*p.sigma
 
     dt = p.T/p.N
-    S = generate_midprice(dt=dt, initial_price=p.s, volatility=p.sigma, time_horizon=p.T, seed=st.session_state.seed, point_count=p.N, path_count=1)
+    S = generate_midprice(dt=dt, initial_price=p.s, volatility=p.sigma, time_horizon=p.T, seed=st.session_state.seed, point_count=p.N, path_count=p.simulations)
     t : np.ndarray = np.linspace(0.0, p.N*dt, p.N)
 
-    return S, t
+    reservations = np.empty_like(S)
+    bid_prices = np.empty_like(S)
+    ask_prices = np.empty_like(S)
+
+
+    for path_idx, path in enumerate(S):
+
+
+        for i in range(len(path)):
+            reservations[path_idx, i] = path[i] - p.q * p.gamma * var * (p.T - t[i])
+
+            db = p.gamma * p.q * var * (p.T - t[i]) + 1/p.gamma * np.log(1 + p.gamma/p.k)
+            da = -p.gamma * p.q * var * (p.T - t[i]) + 1/p.gamma * np.log(1 + p.gamma/p.k)
+
+            bid_prices[path_idx, i] = path[i] - db
+            ask_prices[path_idx, i] = path[i] + da
+
+
+
+    return S, t, reservations, bid_prices, ask_prices
 
 
     # A = np.exp(-k * M/2) / dt
@@ -88,16 +109,36 @@ def run_avellaneda_model(p : model_parameters):
     #     profits.append(profit)
     #     final_qs.append(q)
 
-def plot_model():
+def plot_model(S, t, reservations, bid_prices, ask_prices):
     df = pd.DataFrame(S.T, index=t)
     fig = px.line(df, title='Modeled Stock Price', labels={'index': 'Time (t)', 'value': 'Stock Price', 'variable': 'Path'})
+    df_res = pd.DataFrame(reservations.T, index=t)
+    df_bid = pd.DataFrame(bid_prices.T, index=t)
+    df_ask = pd.DataFrame(ask_prices.T, index=t)
+
+    
+    
+    for col in df_res.columns:
+        fig.add_scatter(x=t, y=df_res[col], mode='lines', 
+                        name=f'Reservation {col}', 
+                        line=dict(dash='dot', width=2))
+
+    for col in df_bid.columns:
+        fig.add_scatter(x=t, y=df_bid[col], mode='lines', 
+                        name=f'Ask {col}', 
+                        line=dict(width=1, color='green'))
+
+    for col in df_ask.columns:
+            fig.add_scatter(x=t, y=df_ask[col], mode='lines', 
+                            name=f'Ask {col}', 
+                            line=dict(width=1, color='red'))
     st.plotly_chart(fig)
 
 
 
 params : model_parameters = get_slider_params()
-S, t = run_avellaneda_model(params)
-plot_model()
+S, t, reservations, bid_prices, ask_prices = run_avellaneda_model(params)
+plot_model(S, t, reservations, bid_prices, ask_prices)
 
 
 
